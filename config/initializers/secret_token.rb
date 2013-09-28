e753142ed0fec9947bf13b3892e567cca35d3505
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-28 12:40:34 -0700

    webapp taht lets uers create playlists that can be added to by sms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playlistr::Application.config.secret_token = '2622043ced42694c37f762c7aa696cd14726dd268cdc539ea2045734e4aa6f29b737c6050c280bab40455adace2d1c29873cd36b12b8342cbf4d87b5218239ba'
