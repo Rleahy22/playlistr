774d991b61f0d828a08dc392c32b305bbfa9fb6d
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-28 14:54:40 -0700

    removed unnecessary comma from rdio.js

--- app/assets/javascripts/rdio.js
+++ app/assets/javascripts/rdio.js
@@ -1,17 +1,19 @@
-console.log(R.ready([callback(ready)]))
-console.log(R.authenticated)
-R.request({
-	method: "createPlaylist",
-	content: {
-		name: "Practice SMSPlaylist",
-		description: "A test version of our app",
-		tracks: "a997982",
-	},
-	success: function(response) {
-		var top = response.result[0]
-		console.log(top.name + " by " + top.artist)
-	}
-	error: function(response) {
-		console.log("error " + response.message)
-	}
+$(document).ready(function() {
+	console.log(R.ready([callback(ready)]))
+	console.log(R.authenticated)
+	R.request({
+		method: "createPlaylist",
+		content: {
+			name: "Practice SMSPlaylist",
+			description: "A test version of our app",
+			tracks: "a997982"
+		},
+		success: function(response) {
+			var top = response.result[0]
+			console.log(top.name + " by " + top.artist)
+		}
+		// error: function(response) {
+		// 	console.log("error " + response.message)
+		// }
+	})
 })
