95918accda8b49124e5873d63bb2211fdf2872db
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-28 15:31:03 -0700

    added authentication and createPlaylist function

--- app/assets/javascripts/rdio.js
+++ app/assets/javascripts/rdio.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
-	console.log(R.ready([callback(ready)]))
-	console.log(R.authenticated)
-	R.request({
-		method: "createPlaylist",
-		content: {
-			name: "Practice SMSPlaylist",
-			description: "A test version of our app",
-			tracks: "a997982"
-		},
-		success: function(response) {
-			var top = response.result[0]
-			console.log(top.name + " by " + top.artist)
-		}
-		// error: function(response) {
-		// 	console.log("error " + response.message)
-		// }
+	$('.sign-in-btn').click(function() {
+		R.authenticate()
+	})
+	$('.create-playlist-btn').click(function() {
+		R.request({
+			method: "createPlaylist",
+			content: {
+				name: "Practice SMSPlaylist",
+				description: "A test version of our app",
+				tracks: "a997982"
+			},
+			success: function(response) {
+				var top = response.result[0]
+				console.log(top.name + " by " + top.artist)
+			}
+			// error: function(response) {
+			// 	console.log("error " + response.message)
+			// }
+		})
 	})
 })
