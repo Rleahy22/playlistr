482fdda8c1a56d4a4e90b0cf493e80e3bbf4db08
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-29 13:40:24 -0700

    removed paren)

--- app/assets/javascripts/rdio.js
+++ app/assets/javascripts/rdio.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		currentPlaylist = data
 	})
 	var addSong = function(key) {
-		$.get('/key'), function(data) {
+		$.get('/key', function(data) {
 			playKey = data
 		}
 		R.ready(function() {
