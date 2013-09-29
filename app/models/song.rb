923a43cb0fb70d3d1cc078551f22bdc6ac5f9ae8
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-29 15:04:01 -0700

    removed paren)

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,10 @@
 class Song < ActiveRecord::Base
   attr_accessible :title
-
+  before_save :normalize
   validates :title, uniqueness: {case_sensitive => false}
+
+  def normalize
+  	self.title.strip.downcase
+  end
+
 end
