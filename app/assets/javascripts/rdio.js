9de5f9301fc980e55b49a4fc1654d16d97516a14
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-28 17:04:32 -0700

    Fixed playlist instantiation to include an artist to avoid error around albums.  Can now create a playlist from index

--- app/assets/javascripts/rdio.js
+++ app/assets/javascripts/rdio.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
-	$('.sign-in-btn').click(function() {
+	$('#sign-in-btn').click(function() {
 		R.authenticate()
 	})
-	$('.create-playlist-btn').click(function() {
-		R.request({
-			method: "createPlaylist",
-			content: {
-				name: "Practice SMSPlaylist",
-				description: "A test version of our app",
-				tracks: "a997982"
-			},
-			success: function(response) {
-				var top = response.result[0]
-				console.log(top.name + " by " + top.artist)
-			}
-			// error: function(response) {
-			// 	console.log("error " + response.message)
-			// }
-		})
+	$('#create-playlist-btn').on('click', function() {
+		R.ready(function() {
+			R.request({
+				method: "createPlaylist",
+				content: {
+					name: "Practice SMSPlaylist",
+					description: "A test version of our app",
+					tracks: "t32961632"
+				},
+				success: function(response) {
+					var top = response.result[0]
+				},
+				error: function(response) {
+					console.log("error " + response.message)
+				}
+			})
+		});
 	})
+})
+)
 })
